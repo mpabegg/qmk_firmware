425708e29adc6310866328e4587e5f5f4921d75c
Matheus Abegg <1006630+mpabegg@users.noreply.github.com>
2023-08-03 21:52:17 -0300

    permissive hold, and add shift to num layer

--- users/mpabegg/mpabegg.c
+++ users/mpabegg/mpabegg.c
@@ -6,8 +6,8 @@
 // Layers
 enum Layers {
   BASE,
-  NAV,
   NUM,
+  NAV,
   EXTRA
 };
 
@@ -18,17 +18,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
          KC_LSFT,  KC_Z, KC_X,    KC_C,    KC_V,             KC_B,    KC_N,   KC_M,            KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
                                   KC_LGUI, LT(NAV, KC_BSPC), KC_LSFT, KC_ENT, LT(NUM, KC_SPC), KC_RALT
     ),
+    [NUM] = LAYOUT_split_3x6_3(
+         KC_GRAVE, KC_1,    KC_2,    KC_3,    KC_4,     KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSLS,
+         XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_QUOT,
+         KC_LSFT,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_RSFT,
+                                     XXXXXXX, MO(EXTRA), XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
+    ),
     [NAV] = LAYOUT_split_3x6_3(
          XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_WH_R, KC_WH_U,   KC_WH_D, KC_WH_L, XXXXXXX, XXXXXXX,
          XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_LEFT, KC_DOWN,   KC_UP,   KC_RGHT, XXXXXXX, XXXXXXX,
          XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_HOME, KC_PGDN,   KC_PGUP, KC_END,  XXXXXXX, XXXXXXX,
                                     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, MO(EXTRA), XXXXXXX
-    ),
-    [NUM] = LAYOUT_split_3x6_3(
-         KC_GRAVE, KC_1,    KC_2,    KC_3,    KC_4,     KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSLS,
-         XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_QUOT,
-         XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-                                     XXXXXXX, MO(EXTRA), XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
     ),
     [EXTRA] = LAYOUT_split_3x6_3(
         KC_F11,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F12,
